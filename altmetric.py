5d0de7bbff4af3a149d54e549d6a4e23ba095173
paulalbert1 <paa2013@med.cornell.edu>
2020-10-29 07:35:46 -0400

    Added flag for "is_oa" field

--- altmetric.py
+++ altmetric.py
@@ -232,6 +232,7 @@ def get_altmetric_record(api_url):
                                     "similar_age_3m",
                                     "higher_than"),
                       get_dict_value(altmetric_record, "altmetric_id"),
+                      get_dict_value(altmetric_record, "is_oa"),                      
                       get_dict_value(altmetric_record, "cited_by_msm_count"),
                       get_dict_value(altmetric_record, "cited_by_posts_count"),
                       get_dict_value(altmetric_record, "cited_by_tweeters_count"),
@@ -285,6 +286,7 @@ def insert_altmetric_records(mysql_db, mysql_cursor, db_records):
             `context-similar_age_3m-pct`,
             `context-similar_age_3m-higher_than`,
             `altmetric_id`,
+            `is_oa`,
             `cited_by_msm_count`,
             `cited_by_posts_count`,
             `cited_by_tweeters_count`,
@@ -311,7 +313,7 @@ def insert_altmetric_records(mysql_db, mysql_cursor, db_records):
             %s, %s, %s, %s, %s,
             %s, %s, %s, %s, %s,
             %s, %s, %s, %s, %s,
-            %s, %s
+            %s, %s, %s
         )
         """
     )
